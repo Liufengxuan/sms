2018fb56b48294853c1ab0c161992bc372884f73
Liufengxuan <813949669@qq.com>
2019-01-26 16:54:30 +0800

    2

--- main.go
+++ main.go
@@ -11,17 +11,19 @@ import (
 )
 
 func main() {
-	//ignoreStaticPath()
+	ignoreStaticPath()
+
 	beego.Run()
 }
 
 func ignoreStaticPath() {
 	//
 	//请求url中没有api字段。
-	//beego.SetStaticPath("group1/M00/", "fdfs/storage_data/data/")
 	beego.InsertFilter("/", beego.BeforeRouter, TransparentStatic)
 	beego.InsertFilter("/*", beego.BeforeRouter, TransparentStatic)
 }
+
+//TransparentStatic da
 func TransparentStatic(ctx *context.Context) {
 	orpath := ctx.Request.URL.Path      //拿到请求的路径
 	beego.Debug("request url:", orpath) //打印路径
@@ -31,6 +33,6 @@ func TransparentStatic(ctx *context.Context) {
 		return
 	}
 
-	beego.Debug(fmt.Sprintf("views" + ctx.Request.URL.Path))                      //实际路径
-	http.ServeFile(ctx.ResponseWriter, ctx.Request, "views"+ctx.Request.URL.Path) //返回静态页面
+	beego.Debug(fmt.Sprintf("page" + ctx.Request.URL.Path))                      //实际路径
+	http.ServeFile(ctx.ResponseWriter, ctx.Request, "page"+ctx.Request.URL.Path) //返回静态页面
 }
